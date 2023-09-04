13499b322d65ceacfda2a760ff3cc06df18d2f76
juan <juand_david2001@hotmail.com>
2023-09-04 16:43:53 -0600

    Se realiza service header y estilos css #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { HeaderServices } from './services/header.service';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HeaderServices } from './services/header.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [HeaderServices],
   bootstrap: [AppComponent]
--- src/app/services/header.service.ts
+++ src/app/services/header.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from "@angular/core";
-import axios
- from "axios";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, throwError, BehaviorSubject } from "rxjs";
+import {retry, catchError} from 'rxjs/operators';
+/*import axios
+ from "axios";*/
+/*export class Header{
+    name:string="";
+    charges:string="";
+    objetive:string="";
+    photo:string="";
+    email:string="";
+    cel:string="";
+    ubication:string="";    
+    social:string="";
+
+    constructor(){};
+}*/
 @Injectable()
 export class HeaderServices{
-    info={
+    constructor(private cliente: HttpClient){};
+    /*info:any={
         name:"",
         charges:"",
         objetive:"",
@@ -12,16 +28,44 @@ export class HeaderServices{
         cel:"",
         ubication:"",
         social:""
+    }*/
+    apiURL:string="http://localhost:8080/header";
+
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type':'aplication/json'
+        })
     }
-    async getHeaderInfo(){
-        try{
+
+    getHeaderInfo():Observable<any>{
+        return this.cliente.get<any>(this.apiURL, this.httpOptions)
+        .pipe(
+            retry(1), //Volver a intentar
+            catchError(this.handleError)
+        )
+
+        /*try{
             this.info=await(await axios.get("http://localhost:8080/header")).data;
             //console.log(this.info.name)
             return this.info;
         }catch(error){
             window.alert(error);
             return null;
+        }*/
+    }
+    handleError(error: any) {
+        let errorMessage = '';
+
+        if(error.error instanceof ErrorEvent){
+            //Get client-side error
+            errorMessage = error.error.message;
+        }else{
+            //Get server-side error
+            errorMessage = `Error code: ${error.status}\n Message: ${error.message}`
         }
+
+        window.alert(errorMessage);
+        return throwError(errorMessage);
     }
     /*getHeaderInfo(){
         axios.get("http://localhost:8080/header")
